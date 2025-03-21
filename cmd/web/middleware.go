a490e444f7012aaf71ac2f07f4cd99a2c168f4f7
DeusExDev <enriquemartin1402@gmail.com>
2025-03-21 06:01:10 +0000

    add middleware to our routes

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 )
 
+// This function will be executed for every request our application receives.
 func commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
@@ -17,6 +18,8 @@ func commonHeaders(next http.Handler) http.Handler {
 	})
 }
 
+// This function is the same as above but it has access to our application struct.
+// So it can use our logger to log information about the request.
 func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var (
@@ -27,5 +30,7 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 		)
 
 		app.logger.Info("received request", "ip", ip, "proto", proto, "method", method, "uri", uri)
+
+		next.ServeHTTP(w, r)
 	})
 }
