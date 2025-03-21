40c5553ba797866abaced221692c4b1931ea7346
DeusExDev <enriquemartin1402@gmail.com>
2025-03-21 05:39:47 +0000

    add middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+)
+
+func commonHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src font.gstatic.com")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-XSS-Protection", "0")
+		w.Header().Set("server", "Go")
+
+		next.ServeHTTP(w, r)
+	})
+}
