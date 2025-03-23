cf7d3cf1f9bfcc733c8489ba877dc122f780ef72
DeusExDev <enriquemartin1402@gmail.com>
2025-03-23 01:26:19 +0000

    add flash message after snippet post

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -12,6 +12,7 @@ type templateData struct {
 	Snippet     models.Snippet
 	Snippets    []models.Snippet
 	Form        any
+	Flash       string
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
