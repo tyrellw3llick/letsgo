39c458ac1d46eab30b6b467a06ae8073fae14583
DeusExDev <enriquemartin1402@gmail.com>
2025-03-23 22:08:23 +0000

    add usermodel insert and singup validation

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,6 +20,7 @@ func (app *application) routes() http.Handler {
 	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
 	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
 	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
 
