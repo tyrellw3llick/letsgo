ca7d264ebef078e7f1efb24c4af726a9af67cc1b
DeusExDev <enriquemartin1402@gmail.com>
2025-03-24 18:57:10 +0000

    unit testing for humanDate func

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -45,7 +45,11 @@ func newTemplateCache() (map[string]*template.Template, error) {
 }
 
 func humanDate(t time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
