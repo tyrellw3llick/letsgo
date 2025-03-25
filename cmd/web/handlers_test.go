23761138d1614738d02302dd9a66fc1edba0a29f
DeusExDev <enriquemartin1402@gmail.com>
2025-03-25 03:05:05 +0000

    add end-to-end testing to ping

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -4,27 +4,28 @@ import (
 	"bytes"
 	"io"
 	"letsgo/internal/assert"
+	"log/slog"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := &application{
+		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
+	}
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	rs, err := ts.Client().Get(ts.URL + "/ping")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	ping(rr, r)
-
-	rs := rr.Result()
-
 	assert.Equal(t, rs.StatusCode, http.StatusOK)
 
 	defer rs.Body.Close()
-
 	body, err := io.ReadAll(rs.Body)
 	if err != nil {
 		t.Fatal(err)
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
+	mux.HandleFunc("GET /ping", ping)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
